c50d65d06cc9d87400acab5b96c3b59ffb1d0e57
Subahana Sulfikar <subahanasulfikar@gmail.com>
2025-03-01 18:43:58 +0530

    befor review

--- hashtable.py
+++ hashtable.py
@@ -1,138 +1,181 @@
-size=10
-# # ------hash() in built method is used in case of any data type of key ----#
-def hash_function(key):
-    return hash(key) % size
-print(hash_function('subahan'))
+# size=10
+# # # ------hash() in built method is used in case of any data type of key ----#
+# def hash_function(key):
+#     return hash(key) % size
+# print(hash_function('subahan'))
 
-# # ------no need of in built hash method for integer
-def hashCode(key):
-    # Return a hash value based on the key
-    return key % size
-print(hashCode(100))
+# # # ------no need of in built hash method for integer
+# def hashCode(key):
+#     # Return a hash value based on the key
+#     return key % size
+# print(hashCode(100))
 
-a=[1,2]
-a.append([1,2])
-print(a)
+# a=[1,2]
+# a.append([1,2])
+# print(a)
 
-#  --------hashtable using list ------#
+# #  --------hashtable using list ------#
 
-class HashTable:
-    def __init__(self,size):
-        self.size = size
-        self.table=[[] for x in range(size)]
-    def hash_code(self,key):
-        return hash(key) % self.size
+# class HashTable:
+#     def __init__(self,size):
+#         self.size = size
+#         self.table=[[] for x in range(size)]
+#     def hash_code(self,key):
+#         return hash(key) % self.size
 
-    def insert(self,key,value):
-        index=self.hash_code(key)
-        for x in self.table[index]:
-            if x[0] == key:
-                x[1] = value
-                return
-        self.table[index].append([key,value])
-    def get(self,key):
-        index=self.hash_code(key)
-        for x in self.table[index]:
-            if x[0] == key:
-                return x[1]
-        return None
-    def remove_key(self, key):
-        index = self.hash_code(key)
-        for x in self.table[index]:
-            if x[0] == key:
-                self.table[index].remove(x)
-                return
-    def print_hash(self):
-        for y in (self.table):
-            print(y)
+#     def insert(self,key,value):
+#         index=self.hash_code(key)
+#         for x in self.table[index]:
+#             if x[0] == key:
+#                 x[1] = value
+#                 return
+#         self.table[index].append([key,value])
+#     def get(self,key):
+#         index=self.hash_code(key)
+#         for x in self.table[index]:
+#             if x[0] == key:
+#                 return x[1]
+#         return None
+#     def remove_key(self, key):
+#         index = self.hash_code(key)
+#         for x in self.table[index]:
+#             if x[0] == key:
+#                 self.table[index].remove(x)
+#                 return
+#     def print_hash(self):
+#         for y in (self.table):
+#             print(y)
 
 
-# -------hashtable using linkedlist -------#
+# # -------hashtable using linkedlist -------#
 
-class Node:
-    def __init__(self,key,value):
-        self.key = key
+# class Node:
+#     def __init__(self,key,value):
+#         self.key = key
+#         self.value = value
+#         self.next = None
+# class HashtableList:
+#     def __init__(self,size):
+#         self.size = size
+#         self.table = [None] * size
+
+#     def hash_code(self,key):
+#         return hash(key) % self.size
+    
+#     def insert(self,key,value):
+#         index= self.hash_code(key)
+#         if self.table[index] is None :
+#             self.table[index] = Node(key,value)
+#             return
+#         else:
+#             start = self.table[index]
+#             while start :
+#                 if start.key == key :
+#                     start.value = value
+#                     return 
+#                 elif start.next is None:
+#                     break
+#                 start = start.next
+#             start.next = Node(key,value)
+#     def display(self):
+#         for i in range(self.size):
+#             print(i,end =' ')
+#             start = self.table[i]
+#             while start:
+#                 print(start.key,start.value,end = ' ')
+#                 start = start.next
+#             print(None)
+    
+#     def get(self,key):
+#         index = self.hash_code(key)
+#         if self.table[index] is None:
+#             print(None)
+#             return
+#         else:
+#             start = self.table[index]
+#             while start :
+#                 if start.key == key :
+#                     print(f"value of {start.key} is {start.value}")
+#                     return 
+#                 elif start.next is None:
+#                     break
+#                 start = start.next
+#             print(None)
+    
+#     def remove_key(self,key):
+#         index = self.hash_code(key)
+#         if self.table[index] is None:
+#             return 
+#         start = self.table[index]
+#         prev = None
+#         while start:
+#             if start.key == key:
+#                 if prev is None:
+#                     self.table[index] = start.next
+#                 else:
+#                     prev.next = start.next
+#                     return True 
+#                 prev = start 
+#                 start = start.next
+#         return False
+
+            
+# ht = HashtableList(10)
+# ht.insert("name", "Alice")
+# ht.insert("age", 25)
+# # print(ht.get("age"))  # Output: 25
+# # ht.remove_key('name')
+# # print(ht.get('name'))
+# ht.insert("city", "New York")
+# ht.insert("email", "alice@example.com")
+# ht.get(25)
+# ht.remove_key('city')
+# ht.display()
+# # # ht.print_hash()
+# # a=[1,5,3,9]
+# # for x,y in enumerate(a):
+# #     print(x,y)
+# print((ht))
+
+
+class Node :
+    def  __init__(self,index,key,value):
+        self.index = index
+        self.key = key 
         self.value = value
         self.next = None
-class HashtableList:
+
+class HashTable :
     def __init__(self,size):
         self.size = size
         self.table = [None] * size
 
-    def hash_code(self,key):
-        return hash(key) % self.size
-    
-    def insert(self,key,value):
-        index= self.hash_code(key)
-        if self.table[index] is None :
-            self.table[index] = Node(key,value)
-            return
+    def hash_function(self,index):
+        return hash(index) % self.size
+
+    def insert(self,index,key,value):
+        index = self.hash_function(index)
+        new_node = Node(index,key,value)
+        if self.table[index] is None:
+            self.table[index] = new_node
         else:
             start = self.table[index]
-            while start :
-                if start.key == key :
+            while start and start.next:
+                if start.key == key:
                     start.value = value
                     return 
-                elif start.next is None:
-                    break
                 start = start.next
-            start.next = Node(key,value)
+            start.next = new_node
+
     def display(self):
         for i in range(self.size):
-            print(i,end =' ')
             start = self.table[i]
-            while start:
-                print(start.key,start.value,end = ' ')
-                start = start.next
-            print(None)
-    
-    def get(self,key):
-        index = self.hash_code(key)
-        if self.table[index] is None:
-            print(None)
-            return
-        else:
-            start = self.table[index]
             while start :
-                if start.key == key :
-                    print(f"value of {start.key} is {start.value}")
-                    return 
-                elif start.next is None:
-                    break
+                print(f"{start.key}:{start.value}",end = ' ,')
                 start = start.next
             print(None)
-    
-    def remove_key(self,key):
-        index = self.hash_code(key)
-        if self.table[index] is None:
-            return 
-        start = self.table[index]
-        prev = None
-        while start:
-            if start.key == key:
-                if prev is None:
-                    self.table[index] = start.next
-                else:
-                    prev.next = start.next
-                    return True 
-                prev = start 
-                start = start.next
-        return False
 
-            
-ht = HashtableList(10)
-ht.insert("name", "Alice")
-ht.insert("age", 25)
-# print(ht.get("age"))  # Output: 25
-# ht.remove_key('name')
-# print(ht.get('name'))
-ht.insert("city", "New York")
-ht.insert("email", "alice@example.com")
-ht.get(25)
-ht.remove_key('city')
-ht.display()
-# # ht.print_hash()
-# a=[1,5,3,9]
-# for x,y in enumerate(a):
-#     print(x,y)
-print((ht))
+h=HashTable(5)
+h.insert(1,'a1','subu')
+h.insert(1,'a2','12')
+h.display()
--- sorting .py
+++ sorting .py
@@ -1,53 +1,32 @@
 # //////////BUBBLE SORT///////// #
-a=[16,15,6,8,5]
+a=[16,15,6,8,5,6]
 
 # /-----without optimaization --------/
 
-def bubble_sort1(a):
-    n=len(a)
+def bubble_sort(a):
+    n = len(a)
     for i in range(n-1):
         for j in range(n-1):
             if a[j] > a[j+1]:
                 a[j],a[j+1] = a[j+1],a[j]
     return a
 
-print('bubble_sort_asc',bubble_sort1(a)) 
+print(bubble_sort(a))
 
-def bubble_sort1_des(a):
-    n=len(a)
+# --------with optimistaion  --------#
+
+def bubble_sort1(a):
+    n = len(a)
     for i in range(n-1):
-        for j in range(n-1):
-            if a[j] < a[j+1]:
-                a[j],a[j+1]=a[j+1],a[j]
+        for j in range(n-1-i):
+            if a[j] > a[j+1]:
+                a[j],a[j+1] = a[j+1],a[j]
     return a
 
-print('bubble_sort_des',bubble_sort1_des(a))
-
-# /---------with optimiztion for j(comparison) --------/
+print(bubble_sort1(a))
 
 def bubble_sort2(a):
     n = len(a)
-    for i in range(n-1):
-        for j in range(n-1-i):
-            if a[j] > a[j+1]:
-                a[j],a[j+1] = a[j+1],a[j]
-    return a
-
-print('bubble_sort2',bubble_sort2(a))
-
-def bubble_sort2_des(a):
-    n = len(a)
-    for i in range(n-1):
-        for j in range(n-1-i):
-            if a[j] < a[j+1]:
-                a[j],a[j+1] = a[j+1],a[j]
-    return a
-
-print('bubble_sort2_dex',bubble_sort2_des(a))
-# ------ fully optimized code for bubble sort -------/
-
-def bubble_sort3(a):
-    n=len(a)
     for i in range(n-1):
         swap = 0
         for j in range(n-1-i):
@@ -58,47 +37,81 @@ def bubble_sort3(a):
             break
     return a
 
-print(bubble_sort3(a))
+print(bubble_sort2(a))
 
-# /////////Insertion Sort////////#
 
-def insertion_sort_des(a):
-    n = len(a)
-    for i in range(1,n):
-        temp = a[i]
-        j = i -1
-        while j >= 0 and a[j] < temp:
-            a[j+1] = a[j]
-            j -= 1
-        a[j+1] = temp
-    return a
+# //////// Insertion Sort //////// #
 
-print(insertion_sort_des(a))
-
-def insertion_sort_asc(a):
+def insertion_sort(a):
     n = len(a)
     for i in range(1,n):
         temp = a[i]
         j = i-1
         while j >= 0 and a[j] > temp:
-            a[j+1]= a[j]
+            a[j+1] = a[j]
             j -=1
         a[j+1] = temp
     return a
 
-print(insertion_sort_asc(a))
+print(insertion_sort(a))
 
-# -------Selection Sort ------#
 
-def selection_sort(a):
-    n=len(a)
+# //////// Selection Sort /////// #
+
+def select_sort(a):
+    n = len(a)
     for i in range(n-1):
-        min_=i
-        for j in range(i+1,n):
-            if a[j] > a[min_]:
-                min_ = j
-        if min_ != i:
-            a[i],a[min_] = a[min_],a[i]
-    return a
+        min_indes = i
+        for j in range (i+1,n):
+            if a[j] < a[min_indes]:
+                min_indes = j
+        if min_indes != i :
+            a[i],a[min_indes] = a[min_indes],a[i]      
+    return a          
 
-print('selection_sort',selection_sort(a)) 
+print(select_sort(a))
+
+#  //////// Quick Sort /////// #
+
+def quick_sort(a):
+    if len(a) <= 1:
+        return a
+    pivot = a[len(a) // 2]
+    left=[x for x in a if x < pivot]
+    mid = [x for x in a if x == pivot]
+    right=[x for x in a if x > pivot]
+    return quick_sort(left) +mid + quick_sort(right)
+
+print(quick_sort(a))
+
+
+# ///////// Merge Sort ////////// #
+
+def merge_sort(a):
+    if len(a) > 1:
+        mid = len(a) // 2
+        left = a[:mid]
+        right = a[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                a[k] = left[i]
+                i += 1
+                k += 1
+            else:
+                a[k] = right[j]
+                j += 1
+                k += 1
+        while i < len(left):
+            a[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            a[k] = right[j]
+            j += 1
+            k += 1
+    return a
+print(merge_sort(a))
